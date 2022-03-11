229259e3115b978468eac9a6e534f8035328c92b
Артём <97228640+ArtemMartynov@users.noreply.github.com>
2022-03-11 18:53:16 +0500

    Add files via upload

---
+++ LR-2/RAD_Graphic_source_files/RAD_GraphicPCH1.h
@@ -0,0 +1,9 @@
+#include <fmx.h>
+#ifdef _WIN32
+#include <tchar.h>
+	typedef int (__stdcall * PVSGruppa)();
+	typedef wchar_t* (__stdcall * PVSImya)();
+	typedef int (__stdcall * PRADGruppa)();
+	typedef wchar_t* (__stdcall * PRADImya)();
+#endif
+
